5612a5b368b7e1cb31e484258c51ebf1995fcdd2
Hope Maloney <emily-hope.maloney@skybettingandgaming.com>
2024-05-22 10:13:49 +0100

    fixes poll for new tix bug

--- src/pollForNewTickets.js
+++ src/pollForNewTickets.js
@@ -1,35 +1,28 @@
 import { getJiraTickets } from "./getTickets.js";
 
-export const isolateNewTickets = async () => {
-const jiraTickets = await getJiraTickets();
+let previousJiraKeyList = [];
 
-const jiraKeyList = Object.keys(Object.groupBy(jiraTickets, ({ key }) => key));
-console.log(jiraKeyList);
+export const isolateNewTickets = async () => {
+
+    try {
+      const currentKeyList = Object.keys(
+            Object.groupBy(await getJiraTickets(), ({ key }) => key)
+          );
+      const newKeys = identifyNewTickets(previousJiraKeyList, currentKeyList);
+      if (newKeys.length > 0) {
+        console.log(`new tickets since last`);
+        newKeys.forEach((ticket) => console.log(`New ticket: ${ticket}`));
+      } else {
+        console.log(`No new tickets`);
+      }
+      previousJiraKeyList = currentKeyList;
+    } catch (error) {
+      console.error(`error fetching new ticket keys`, error);
+    }
+  };
 
 function identifyNewTickets(previousJiraKeyList, newJiraKeyList) {
   return newJiraKeyList.filter(
     (ticket) => !previousJiraKeyList.includes(ticket)
   );
-}
-
-function pollForNewTickets() {
-  let previousJiraKeyList = [];
-
-  setInterval(async () => {
-    try {
-      const currentKeyList = await getJiraTickets();
-      const newKeys = identifyNewTickets(previousJiraKeyList, currentKeyList);
-      if (newKeys.length > 0) {
-        console.log(`new tickets since last`);
-        newKeys.forEach((ticket) => console.log(`New ticket: ${ticket.key}`));
-      } else {
-        console.log(`No new tickets`);
-      }
-      previousJiraKeyList = jiraTickets;
-    } catch (error) {
-      console.error(`error fetching new ticket keys`, error);
-    }
-  }, 30000);
-}
-pollForNewTickets();
 }
